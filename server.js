0ef61574e4ddd886af6a953d7c25aca971337ef4
BilalMir135 <bilalmir135@gmail.com>
2020-10-30 02:18:28 +0500

    Connect DB & Test Routes

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
 const express = require('express');
 const app = express();
+const connectDB = require('./config/db');
+
+//connect DB
+connectDB();
 
 app.get('/', (req, res) => res.send('API Running'));
+
+//define routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/posts', require('./routes/api/posts'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
