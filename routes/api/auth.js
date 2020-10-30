313f225bd25dfcb66dd809e56d7dcfb999f36df0
BilalMir135 <bilalmir135@gmail.com>
2020-10-31 03:00:22 +0500

    Configure JWT

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,9 +1,67 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middlewares/auth');
+const User = require('../../models/User');
+const { body, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 
 // @route  GET api/auth
 // @desc   Test route
 // @access Public
-router.get('/', (req, res) => res.send('Auth route'));
+router.get('/', auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select('-password');
+    res.json(user);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Server Error');
+  }
+});
+
+// @route  POST api/auth
+// @desc   Authenticate & get token
+// @access Public
+router.post(
+  '/',
+  [body('email', 'Please insert a valid email').isEmail(), body('password', 'Password is requires').exists()],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email, password } = req.body;
+
+    try {
+      let user = await User.findOne({ email });
+      if (!user) {
+        return res.status(400).json({ errors: [{ msg: 'Invalid Credentials' }] });
+      }
+
+      const isMatch = await bcrypt.compare(password, user.password);
+
+      if (!isMatch) {
+        return res.status(400).json({ errors: [{ msg: 'Invalid Credentials' }] });
+      }
+
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      jwt.sign(payload, config.get('jwtSecret'), { expiresIn: 360000 }, (err, token) => {
+        if (err) throw err;
+        res.json({ token });
+      });
+    } catch (err) {
+      console.error(err);
+      res.status(500).send('Server Error');
+      process.exit(1);
+    }
+  }
+);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const connectDB = require('./config/db');
 //connect DB
 connectDB();
 
+//init middleware
+app.use(express.json({ extended: false }));
+
 app.get('/', (req, res) => res.send('API Running'));
 
 //define routes
